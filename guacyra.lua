c1d986b4841b4fdffa9f4af75698a962898be1f9
Vinicius Mello <vinicius.moreira.mello@gmail.com>
2022-02-13 11:08:10 -0300

    Prime

--- guacyra.lua
+++ guacyra.lua
@@ -1,162 +1,9 @@
 
--- Number Theory
 local floor, infinite, random = math.floor, math.huge, math.random
 local abs, max, min, ceil = math.abs, math.max, math.min, math.ceil
-
-local function gcd(a, b)
-  while b ~= 0 do a, b = b, a % b end
-  return abs(a)
-end
-
-local function invmodp(a, p)
-  local t, newt = 0, 1
-  local r, newr = p, a
-  while newr ~= 0 do
-    local quotient = floor(r/newr)
-    t, newt = newt, t-quotient*newt
-    r, newr = newr, r-quotient*newr
-  end
-  if r > 1 then
-      error "a is not invertible"
-  end
-  if t < 0 then
-      t = t+p
-  end
-  return t
-end
-
-local function isInt(a) return type(a) == 'number' and a == floor(a) end
-
-local function binomial(n, k)
-  if k > n then return nil end
-  if k > n / 2 then k = n - k end
-  local numer, denom = 1, 1
-  for i = 1, k do
-    numer = numer * (n - i + 1)
-    denom = denom * i
-  end
-  return floor(numer / denom) -- lua 5.3
-end
-
-local function factorial(n)
-  local r = 1
-  for i=1,n do
-    r = r*i
-  end
-  return r
-end
-
---- Calculate the modular power for any exponent.
-local function fmodpow(bse, exp, mod)
-  bse = bse % mod
-  local prod = 1
-  while exp > 0 do
-    if exp % 2 == 1 then prod = prod * bse % mod end
-    exp = floor(exp / 2)
-    bse = (bse * bse) % mod
-  end
-  return prod
-end
-
-local function witnesses(n)
-  if n < 1373653 then
-    return 2, 3
-  elseif n < 4759123141 then
-    return 2, 7, 61
-  elseif n < 2152302898747 then
-    return 2, 3, 5, 7, 11
-  elseif n < 3474749660383 then
-    return 2, 3, 5, 7, 11, 13
-  else
-    return 2, 325, 9375, 28178, 450775, 9780504, 1795265022
-  end
-end
-
---- Given a number n, returns numbers r and d such that 2^r*d+1 == n
---- Miller-Rabin primality test
-local function miller_rabin(n, ...)
-  local s, d = 0, n - 1
-  while d % 2 == 0 do d, s = d / 2, s + 1 end
-  for i = 1, select('#', ...) do
-    local witness = select(i, ...)
-    if witness >= n then break end
-    local x = fmodpow(witness, d, n)
-    if (x ~= 1) then
-      local t = s
-      while x ~= n - 1 do
-        t = t - 1
-        if t <= 0 then return false end
-        x = (x * x) % n
-        if x == 1 then return false end
-      end
-    end
-  end
-  return true
-end
-
-local mrthreshold = 1e3
-
-local primes = setmetatable({
-  2, 3 --[[just hard-code the even special case and following number]]
-}, {
-  __index = function(self, index)
-    if type(index) == 'number' then
-      for i = #self, index - 1 do local dummy = self[i] end -- Precalculate previous primes to avoid building up a stack
-      for candidate = self[index - 1] + 2 --[[All primes >2 are odd]] , infinite do
-        if index > mrthreshold then
-          if miller_rabin(candidate, witnesses(candidate)) then
-            rawset(self, index, candidate)
-            return candidate
-          end
-        else
-          local half = floor(candidate / 2)
-          for i = 1, index - 1 do
-            local div = self[i]
-            if div > half then
-              rawset(self, index, candidate);
-              return candidate
-            end -- A number can't possibly be divisible by something greater than its half
-            if candidate % div == 0 then break end -- Candidate is divisible by a prime, this not prime itself
-          end
-        end
-      end
-    end
-  end
-})
-
-local function factorize(subject)
-  if subject == 1 then
-    return -- Can be ommitted for implicit return ;)
-  elseif subject > 0 then
-    for i = 1, infinite do
-      local candidate = primes[i]
-      if subject % candidate == 0 then
-        return candidate, factorize(subject / candidate)
-      end
-    end
-  else
-    return nil,
-           "Can't be bothered to look up if negative numbers have a prime factorization"
-  end
-end
-
-local function factorization(n)
-  local a = {factorize(n)}
-  local count = 0
-  local cur = a[1]
-  local r = {}
-  for i = 1, #a + 1 do
-    local ai = a[i]
-    if ai == cur then
-      count = count + 1
-    else
-      r[#r + 1] = {cur, count}
-      cur = ai
-      count = 1
-    end
-  end
-  return r
-end
+local gcd, invmodp, isInt, binomial, factorial
+local fmodpow, primes
+local factorize, factorization
 
 -- Kernel
 local guacyra = {}
@@ -866,6 +713,15 @@ function(a, b)
     return a
   end
   return b
+end)
+local Prime = Symbols('Prime', guacyra)
+Rule(Prime(_{n=Int}),
+function(n)
+  n = numericValue(n)
+  if n>0 then
+    return Int(primes[n])
+  end
+  return nil
 end)
 local Floor, Round, Ceil =
   Symbols('Floor Round Ceil', guacyra)
@@ -2599,6 +2455,164 @@ guacyra.import = function()
   end
   _G['Symbols'] = Symbols
   _G['Rule'] = Rule
+  return guacyra
+end
+
+-- Number Theory
+
+gcd = function(a, b)
+  while b ~= 0 do a, b = b, a % b end
+  return abs(a)
+end
+
+invmodp = function(a, p)
+  local t, newt = 0, 1
+  local r, newr = p, a
+  while newr ~= 0 do
+    local quotient = floor(r/newr)
+    t, newt = newt, t-quotient*newt
+    r, newr = newr, r-quotient*newr
+  end
+  if r > 1 then
+      error "a is not invertible"
+  end
+  if t < 0 then
+      t = t+p
+  end
+  return t
+end
+
+isInt = function(a) return type(a) == 'number' and a == floor(a) end
+
+binomial = function(n, k)
+  if k > n then return nil end
+  if k > n / 2 then k = n - k end
+  local numer, denom = 1, 1
+  for i = 1, k do
+    numer = numer * (n - i + 1)
+    denom = denom * i
+  end
+  return floor(numer / denom) -- lua 5.3
+end
+
+factorial = function(n)
+  local r = 1
+  for i=1,n do
+    r = r*i
+  end
+  return r
+end
+
+--- Calculate the modular power for any exponent.
+fmodpow = function(bse, exp, mod)
+  bse = bse % mod
+  local prod = 1
+  while exp > 0 do
+    if exp % 2 == 1 then prod = prod * bse % mod end
+    exp = floor(exp / 2)
+    bse = (bse * bse) % mod
+  end
+  return prod
+end
+
+local function witnesses(n)
+  if n < 1373653 then
+    return 2, 3
+  elseif n < 4759123141 then
+    return 2, 7, 61
+  elseif n < 2152302898747 then
+    return 2, 3, 5, 7, 11
+  elseif n < 3474749660383 then
+    return 2, 3, 5, 7, 11, 13
+  else
+    return 2, 325, 9375, 28178, 450775, 9780504, 1795265022
+  end
+end
+
+--- Given a number n, returns numbers r and d such that 2^r*d+1 == n
+--- Miller-Rabin primality test
+local function miller_rabin(n, ...)
+  local s, d = 0, n - 1
+  while d % 2 == 0 do d, s = d / 2, s + 1 end
+  for i = 1, select('#', ...) do
+    local witness = select(i, ...)
+    if witness >= n then break end
+    local x = fmodpow(witness, d, n)
+    if (x ~= 1) then
+      local t = s
+      while x ~= n - 1 do
+        t = t - 1
+        if t <= 0 then return false end
+        x = (x * x) % n
+        if x == 1 then return false end
+      end
+    end
+  end
+  return true
+end
+
+local mrthreshold = 1e3
+
+primes = setmetatable({
+  2, 3 --[[just hard-code the even special case and following number]]
+}, {
+  __index = function(self, index)
+    if type(index) == 'number' then
+      for i = #self, index - 1 do local dummy = self[i] end -- Precalculate previous primes to avoid building up a stack
+      for candidate = self[index - 1] + 2 --[[All primes >2 are odd]] , infinite do
+        if index > mrthreshold then
+          if miller_rabin(candidate, witnesses(candidate)) then
+            rawset(self, index, candidate)
+            return candidate
+          end
+        else
+          local half = floor(candidate / 2)
+          for i = 1, index - 1 do
+            local div = self[i]
+            if div > half then
+              rawset(self, index, candidate);
+              return candidate
+            end -- A number can't possibly be divisible by something greater than its half
+            if candidate % div == 0 then break end -- Candidate is divisible by a prime, this not prime itself
+          end
+        end
+      end
+    end
+  end
+})
+
+factorize = function(subject)
+  if subject == 1 then
+    return -- Can be ommitted for implicit return ;)
+  elseif subject > 0 then
+    for i = 1, infinite do
+      local candidate = primes[i]
+      if subject % candidate == 0 then
+        return candidate, factorize(subject / candidate)
+      end
+    end
+  else
+    return nil,
+           "Can't be bothered to look up if negative numbers have a prime factorization"
+  end
+end
+
+factorization = function(n)
+  local a = {factorize(n)}
+  local count = 0
+  local cur = a[1]
+  local r = {}
+  for i = 1, #a + 1 do
+    local ai = a[i]
+    if ai == cur then
+      count = count + 1
+    else
+      r[#r + 1] = {cur, count}
+      cur = ai
+      count = 1
+    end
+  end
+  return r
 end
 
 return guacyra
